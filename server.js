061896f0338e2b96dc276d918e0de17d9a141fb0
rjanjanam <ratna.janjanam@rms.com>
2019-08-20 21:24:37 -0700

    First commit and push , Recipes NodeJS Express and MonGoDB

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ let categories = menuFactory.getCategories();
 var namesObj = [];
 
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 // GET response for '/'
 app.get('/', function (req, res) {
